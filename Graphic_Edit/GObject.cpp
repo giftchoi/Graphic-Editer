87f24ae12faf756b204480910011755ac976e753
giftchoi <giftchoi@naver.com>
2015-05-26 01:00:06 +0900

    setPoint 함수(3개),getPoint함수, move함수 추가, GObject멤버변수 추가

--- Graphic_Edit/GObject.cpp
+++ Graphic_Edit/GObject.cpp
@@ -10,6 +10,8 @@
 
 GObject::GObject()
 {
+	m_Bold = 0;
+	m_ID = 0;
 }
 
 GObject::~GObject()
@@ -18,3 +20,40 @@ GObject::~GObject()
 
 
 // GObject member functions
+
+
+void GObject::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{	// storing code
+	}
+	else
+	{	// loading code
+	}
+}
+
+
+void GObject::setPoint(int left, int top, int right, int bottom)
+{
+}
+
+
+void GObject::setPoint(int right, int bottom)
+{
+}
+
+
+void GObject::setPoint(CPoint point)
+{
+}
+
+
+void GObject::move(int dx, int dy)
+{
+}
+
+
+CPoint GObject::getPoint()
+{
+	return CPoint();
+}
--- Graphic_Edit/GObject.h
+++ Graphic_Edit/GObject.h
@@ -1,12 +1,28 @@
 #pragma once
 
 // GObject command target
+enum Type { SELECTED, LINE, POLYLINE , ELLIPSE, RECTANGLE, TEXT,GROUP};
 
 class GObject : public CObject
 {
 public:
 	GObject();
 	virtual ~GObject();
+	virtual void Serialize(CArchive& ar);
+	
+	virtual void setPoint(int left, int top, int right, int bottom);
+	void setPoint(int right, int bottom);
+	void setPoint(CPoint point);
+	virtual void move(int dx, int dy);
+	CPoint getPoint();
+	
+	Type type;
+	int m_Bold;
+	COLORREF m_LineColor;
+	CPoint m_StartPoint;
+	CPoint m_EndPoint;
+	CPoint m_OriginPoint;
+	CRgn m_rgn;
+	BOOL m_selected;
+	int m_ID;
 };
-
-
