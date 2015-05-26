4f6c81aca9b5a98ea883e6992f71cf2978654f26
rkfqlwjstk <tkacjstoro@naver.com>
2015-05-26 19:39:45 +0900

    리소스뷰 UI 만듬

--- Graphic_Edit/Resource.h
+++ Graphic_Edit/Resource.h
@@ -1,49 +1,65 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Graphic_Edit.rc
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// Graphic_Edit.rc에서 사용되고 있습니다.
 //
-#define IDR_PASTE_MENU				151
-#define IDR_WINDOWS_MENU			152
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define ID_WRITE_PASTEASHYPERLINK	32770
-#define IDB_WRITESMALL				110
-#define IDB_WRITELARGE				111
-#define IDB_MAIN					112
-#define IDB_BUTTONS					113
-#define IDB_FILELARGE				114
-#define IDB_FILESMALL				115
-#define IDR_POPUP_EDIT				119
-#define ID_STATUSBAR_PANE1			120
-#define ID_STATUSBAR_PANE2			121
-#define IDS_STATUS_PANE1			122
-#define IDS_STATUS_PANE2			123
-#define IDR_MAINFRAME				128
-#define IDR_MAINFRAME_256			129
-#define IDR_Graphic_EditTYPE				130
-#define IDR_THEME_MENU				200
-#define ID_SET_STYLE				201
-#define ID_VIEW_APPLOOK_WIN_2000	205
-#define ID_VIEW_APPLOOK_OFF_XP		206
-#define ID_VIEW_APPLOOK_WIN_XP		207
-#define ID_VIEW_APPLOOK_OFF_2003	208
-#define ID_VIEW_APPLOOK_VS_2005		209
-#define ID_VIEW_APPLOOK_VS_2008		210
-#define ID_VIEW_APPLOOK_OFF_2007_BLUE	215
-#define ID_VIEW_APPLOOK_OFF_2007_BLACK	216
-#define ID_VIEW_APPLOOK_OFF_2007_SILVER	217
-#define ID_VIEW_APPLOOK_OFF_2007_AQUA	218
-#define ID_VIEW_APPLOOK_WINDOWS_7	219
-#define IDS_EDIT_MENU				306
-#define IDR_RIBBON             			307
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDB_WRITESMALL                  110
+#define IDB_WRITELARGE                  111
+#define IDB_MAIN                        112
+#define IDB_BUTTONS                     113
+#define IDB_FILELARGE                   114
+#define IDB_FILESMALL                   115
+#define IDR_POPUP_EDIT                  119
+#define ID_STATUSBAR_PANE1              120
+#define ID_STATUSBAR_PANE2              121
+#define IDS_STATUS_PANE1                122
+#define IDS_STATUS_PANE2                123
+#define IDR_MAINFRAME                   128
+#define IDR_MAINFRAME_256               129
+#define IDR_Graphic_EditTYPE            130
+#define IDR_PASTE_MENU                  151
+#define IDR_WINDOWS_MENU                152
+#define IDR_THEME_MENU                  200
+#define ID_SET_STYLE                    201
+#define ID_VIEW_APPLOOK_WIN_2000        205
+#define ID_VIEW_APPLOOK_OFF_XP          206
+#define ID_VIEW_APPLOOK_WIN_XP          207
+#define ID_VIEW_APPLOOK_OFF_2003        208
+#define ID_VIEW_APPLOOK_VS_2005         209
+#define ID_VIEW_APPLOOK_VS_2008         210
+#define ID_VIEW_APPLOOK_OFF_2007_BLUE   215
+#define ID_VIEW_APPLOOK_OFF_2007_BLACK  216
+#define ID_VIEW_APPLOOK_OFF_2007_SILVER 217
+#define ID_VIEW_APPLOOK_OFF_2007_AQUA   218
+#define ID_VIEW_APPLOOK_WINDOWS_7       219
+#define IDS_EDIT_MENU                   306
+#define IDR_RIBBON                      307
+#define ID_WRITE_PASTEASHYPERLINK       32770
+#define ID_SELECTED                     32771
+#define ID_LINE                         32772
+#define ID_POLYLINE                     32774
+#define ID_ELLIPSE                      32775
+#define ID_RECTANGLE                    32776
+#define ID_TEXT                         32777
+#define ID_LINECOLOR                    32779
+#define ID_RGNCOLOR                     32780
+#define ID_POINTDELETE                  32784
+#define ID_DELETE                       32785
+#define ID_LINEPATTERN                  32786
+#define ID_REGIONPATTERN                32787
+#define ID_FONT                         32788
+#define ID_GROUPING                     32789
+#define ID_BUTTON17                     32790
+#define ID_GROUPRELEASE                 32790
 
-// 다음은 새 개체에 사용할 기본값입니다.
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	310
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		310
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        310
+#define _APS_NEXT_COMMAND_VALUE         32791
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
