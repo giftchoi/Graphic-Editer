d86502b6338706914f62da4b3a75e3f3c6e3252c
rkfqlwjstk <tkacjstoro@naver.com>
2015-05-26 21:24:30 +0900

    Ellipse 멤버 변수, 멤버함수 추가

--- Graphic_Edit/Ellipse.cpp
+++ Graphic_Edit/Ellipse.cpp
@@ -10,6 +10,8 @@
 
 Ellipse::Ellipse()
 {
+	m_linepattern = 0;
+	m_rgnpattern = 0;
 }
 
 Ellipse::~Ellipse()
@@ -18,3 +20,24 @@ Ellipse::~Ellipse()
 
 
 // Ellipse member functions
+
+
+void Ellipse::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{	// storing code
+	}
+	else
+	{	// loading code
+	}
+}
+
+
+void Ellipse::setPoint(int left, int top, int right, int bottom)
+{
+}
+
+
+void Ellipse::move(int dx, int dy)
+{
+}
--- Graphic_Edit/Ellipse.h
+++ Graphic_Edit/Ellipse.h
@@ -8,6 +8,21 @@ class Ellipse : public GObject
 public:
 	Ellipse();
 	virtual ~Ellipse();
+	virtual void Serialize(CArchive& ar);
+	virtual COLORREF getRgncolor(){ return m_rgncolor; }
+	virtual void setRgncolor(COLORREF color){ m_rgncolor = color; }
+
+	void setLinePattern(int pattern){ m_linepattern = pattern; }
+	void setRgnPattern(int pattern){ m_rgnpattern = pattern; }
+	virtual void setPoint(int left, int top, int right, int bottom);
+	virtual void move(int dx, int dy);
+public:
+	
+	COLORREF m_rgncolor;
+	int m_linepattern;
+	int m_rgnpattern;
+	CRect selectedRect[8];
+	
 };
 
 
